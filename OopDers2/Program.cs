f3b7616565e6a2f566fc65e71e8909a3b0669e3a
Büşra Nur Kuşçu <191118034@samsun.edu.tr>
2020-12-02 15:35:47 +0300

    Add project files.

---
+++ OopDers2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OopDers2
+{
+    class Program
+    {
+        private int [,] _Data { get; set; }
+        public Program(int[,] data)
+        {
+             for(int i= 0; i< _Data.GetLength(0); i++)
+            {
+                for(int j=0; j<_Data.GetLength(1); i++)
+                {
+                    _Data[i, j] = data[i, j];
+                }
+            }
+        }
+       
+    }
+}
